3c74b2c0ad98b655736b4df1a46d0e3c9ef354bc
backend-dev <backend-dev@hiclaw.local>
2026-04-01 14:14:13 +0800

    refactor: 清空代码实现，只保留 OpenSpec 规范和开发规范

--- rental-property/src/main/java/com/rental/property/mapper/BuildingMapper.java
+++
@@ -1,9 +0,0 @@
-package com.rental.property.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.rental.property.entity.Building;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface BuildingMapper extends BaseMapper<Building> {
-}
--- rental-property/src/main/java/com/rental/property/mapper/PropertyMapper.java
+++
@@ -1,9 +0,0 @@
-package com.rental.property.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.rental.property.entity.Property;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PropertyMapper extends BaseMapper<Property> {
-}
